29ba36904ace0bc618d22752685f5381b682ec06
josecoll <jose.coll@r3cev.com>
2017-06-05 11:50:33 +0100

    WIP - Kotlin Comparable Generics error

--- finance/src/main/kotlin/net/corda/contracts/testing/DummyDealContract.kt
+++ finance/src/main/kotlin/net/corda/contracts/testing/DummyDealContract.kt
@@ -7,7 +7,12 @@ import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.crypto.*
 import net.corda.core.identity.AnonymousParty
 import net.corda.core.identity.Party
+import net.corda.core.schemas.MappedSchema
+import net.corda.core.schemas.PersistentState
+import net.corda.core.schemas.QueryableState
 import net.corda.core.transactions.TransactionBuilder
+import net.corda.schemas.DummyDealStateSchemaV1
+import net.corda.schemas.DummyLinearStateSchemaV1
 import java.security.PublicKey
 
 class DummyDealContract : Contract {
@@ -20,13 +25,27 @@ class DummyDealContract : Contract {
             override val participants: List<PublicKey> = listOf(),
             override val linearId: UniqueIdentifier = UniqueIdentifier(),
             override val ref: String,
-            override val parties: List<AnonymousParty> = listOf()) : DealState {
+            override val parties: List<AnonymousParty> = listOf()) : DealState, QueryableState {
+
         override fun isRelevant(ourKeys: Set<PublicKey>): Boolean {
-            return participants.any { it.containsAny(ourKeys) }
+            return true
+//            return participants.any { it.containsAny(ourKeys) }
         }
 
         override fun generateAgreement(notary: Party): TransactionBuilder {
             throw UnsupportedOperationException("not implemented")
+        }
+
+        override fun supportedSchemas(): Iterable<MappedSchema> = listOf(DummyDealStateSchemaV1)
+
+        override fun generateMappedObject(schema: MappedSchema): PersistentState {
+            return when (schema) {
+                is DummyDealStateSchemaV1 -> DummyDealStateSchemaV1.PersistentDummyDealState(
+                        uid = linearId,
+                        dealReference = ref
+                )
+                else -> throw IllegalArgumentException("Unrecognised schema $schema")
+            }
         }
     }
 }
